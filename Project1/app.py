fb5d9e6f7743faede71d0245b3e8f9b40ec2499b
anuragpathak2608 <anuragpathak2608@gmail.com>
2020-03-26 22:23:55 +0530

    added few functionalites

--- Project1/app.py
+++ Project1/app.py
@@ -1,5 +1,5 @@
 #Imported flask library for web development
-from flask import Flask
+from flask import Flask, render_template
 from models import Base
 
 #Imported db session form setupdb for intracting with db
@@ -19,12 +19,19 @@ from flask import abort
 #Create instance of flask
 app = Flask(__name__)
 
-#Create route
-@app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/test/<id>")
+def test(id):
+    task_by_id = db_session.query(Tasks).get(id)
+    #if task_by_id is None:
+       # return {"msg": "Task not found"}
+    #print(task_by_id.serialize())
+    data = task_by_id.serialize()
+    print(data['id'])
+    #return 'none'
+    return render_template('home.html', data='data')
 
 
+#API to list all the tasks which are not deleted(includes complete/incomplete)
 @app.route("/tasks")
 def get_all_tasks():
     tasks = db_session.query(Tasks).all()
@@ -34,6 +41,8 @@ def get_all_tasks():
             task_list.append(task.serialize())
     return jsonify(task_list)
 
+
+#API to list all the deleted tasks
 @app.route("/tasks/deleted")
 def get_all_deleted_tasks():
     all_del_task = db_session.query(Tasks).all()
@@ -45,22 +54,23 @@ def get_all_deleted_tasks():
         if task.tisdeleted == False:
             continue
         del_task_list.append(task.serialize())
-        #else:
-            #return {"msg": "no deleted task found"}
     return jsonify(del_task_list)
 
 
-@app.route("/tasks", methods = ['POST'])
+#API to create the task
+@app.route("/tasks", methods=['POST'])
 def create_new_task():
     #In post operation we first fetch the json received from client
     received_json = request.get_json()
     #Convert that Json into you object
     tasks_object = Tasks(received_json)
+    tasks_object.tcreatedondate = datetime.now()
     db_session.add(tasks_object)
     db_session.commit()
-    return {"id": tasks_object.tid, "msg": "task created"}
+    return {"id": tasks_object.tid, "msg": "task created"}, 201
 
 
+#API to get the task by is ID
 @app.route("/tasks/<id>")
 def get_task_by_id(id):
     task_by_id = db_session.query(Tasks).get(id)
@@ -69,6 +79,7 @@ def get_task_by_id(id):
     return jsonify(task_by_id.serialize())
 
 
+#API to hard delete(is deleted flag is True) the task
 @app.route("/tasks/hard/<id>", methods=['DELETE'])
 def hard_delete(id):
     delete_obj = db_session.query(Tasks).get(id)
@@ -83,7 +94,7 @@ def hard_delete(id):
         return {"msg": "task can not be deleted first soft delete the task"}
 
 
-
+#API to soft delete(sets isdeleted flag to True) the task
 @app.route("/tasks/<id>", methods=['DELETE'])
 def soft_delete(id):
     task_to_delete =  db_session.query(Tasks).get(id)
@@ -96,16 +107,66 @@ def soft_delete(id):
     return {"id": task_to_delete.tid, "msg": "task deleted successfully"}
 
 
+#API to mark the task as completed.
+@app.route("/tasks/<id>/done")
+def task_as_done(id):
+    task_obj = db_session.query(Tasks).get(id)
+    if task_obj is None:
+        return {"msg": "task not found"}
 
+    if task_obj.tisdeleted is True:
+        return {"msg": "You can not complete the tasks which are deleted"}
+
+    task_obj.tisdone = True
+    db_session.commit()
+    return {"id": id, "msg": "task is completed"}
+
+
+#API to show all completed task.
+@app.route("/tasks/completed")
+def get_all_completed_tasks():
+    all_completed_tasks = db_session.query(Tasks).all()
+    if all_completed_tasks is None:
+        return {"msg": "no completed task found"}
+    task_list = []
+    for task in all_completed_tasks:
+        if task.tisdone is True and task.tisdeleted is False:
+            task_list.append(task.serialize())
+    return jsonify(task_list)
+
+
+#API to update the task.
+#can be used to mark the task complete or delete the task
+@app.route("/tasks/<id>", methods=['PUT'])
+def upadate_task(id):
+    task = Tasks.query.get(id)
+    ttitle = request.json['ttitle']
+    tdesc = request.json['tdesc']
+    tcreatedbyuser = request.json['tcreatedbyuser']
+    tisdeleted = request.json['tisdeleted']
+    tisdone = request.json['tisdeleted']
+
+    task.tisdeleted = tisdeleted
+    task.ttitle = ttitle
+    task.tdesc = tdesc
+    task.tcreatedbyuser = tcreatedbyuser
+    task.tisdone = tisdone
+
+    db_session.commit()
+    return {"id": id, "msg": "task updated successfully"}
+
+
+#API to create the user.
 @app.route("/users", methods=['POST'])
 def add_user():
     user_details = request.get_json()
     user_details_object = Users(user_details)
     db_session.add(user_details_object)
     db_session.commit()
-    return {"id": user_details_object.uid, "msg": "user created successfully"}
+    return {"id": user_details_object.uid, "msg": "user created successfully"}, 201
 
 
+#API to delete the perticluar user
 @app.route("/users/<id>", methods=['DELETE'])
 def del_user(id):
     delete_user = db_session.query(Users).get(id)
@@ -116,6 +177,7 @@ def del_user(id):
     return {"id": id, "msg": "user deleted successfully"}
 
 
+#API to get the details of the user by its ID.
 @app.route("/users/<id>")
 def get_user_by_id(id):
     user_object = db_session.query(Users).get(id)
@@ -124,6 +186,7 @@ def get_user_by_id(id):
     return jsonify(user_object.serialize())
 
 
+#API to list all the users
 @app.route("/users")
 def get_all_users():
     get_all_user_object = db_session.query(Users).all()
--- Project1/models.py
+++ Project1/models.py
@@ -9,6 +9,8 @@ from sqlalchemy.pool import StaticPool
 Base = declarative_base()
 
 
+#Each class in model.py is table.
+#Each data member in class is a column
 class Tasks(Base):
     __tablename__ = "tasks"
     tid = Column("tid", Integer, nullable=False, primary_key=True, autoincrement=True)
@@ -19,15 +21,19 @@ class Tasks(Base):
     tisdeleted = Column("tisdeleted", Boolean,nullable=False, default=False)
     tisdone = Column("tisdone", Boolean, nullable=True, default=False)
 
+
+    #Tasks class constructor used for taking the input from JSOn
     def __init__(self, data):
-       # self.tid = data["tid"]
+       #self.tid = data["tid"]
         self.ttitle = data["ttitle"]
         self.tdesc = data["tdesc"]
         self.tcreatedbyuser = data["tcreatedbyuser"]
-        self.tcreatedondate = data["tcreatedondate"]
+       #self.tcreatedondate = data["tcreatedondate"]
         self.tisdeleted = data["tisdeleted"]
         self.tisdone = data["tisdone"]
 
+
+    #Task Class serializer to convert the Pythton object back to the the json format
     def serialize(self):
         return {
             "id": self.tid,
@@ -40,17 +46,22 @@ class Tasks(Base):
          }
 
 
+#user table with columns details.
 class Users(Base):
     __tablename__ = "users"
     uid = Column("uid", Integer,nullable=False, primary_key=True, autoincrement=True)
     uname = Column("uname", String(30), nullable=False)
     uemail = Column("uemail", String(50))
 
+
+    #Users call constructor to read the input from the json.
     def __init__(self, user_data):
-       # self.uid = user_data["uid"]
+        # self.uid = user_data["uid"]
         self.uname = user_data["uname"]
         self.uemail = user_data["uemail"]
 
+
+    #user clalss serializer to convert python object back to the json format
     def serialize(self):
         return {
             "id": self.uid,
@@ -59,6 +70,7 @@ class Users(Base):
         }
 
 
+#database connection
 if __name__ == "__main__":
     engine = create_engine("sqlite:///todo.db", connect_args={"check_same_thread": False}, poolclass=StaticPool)
     Base.metadata.create_all(engine)
